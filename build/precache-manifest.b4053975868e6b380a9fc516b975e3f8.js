6039e55a7865280677d2205a02385b1f847354a4
Peeyush Maharshi <peeyush.maharshi@genpact.com>
2018-12-16 18:57:57 +0530

    initial commit

---
+++ build/precache-manifest.b4053975868e6b380a9fc516b975e3f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c69eeec9976245c845b",
+    "url": "/static/js/main.4c69eeec.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "4c69eeec9976245c845b",
+    "url": "/static/css/main.32899979.chunk.css"
+  },
+  {
+    "revision": "5a65e43ef3d69a06199819641610403a",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p className="App-intro">
+            Howdy from Peeyush - again and again - four times 
           </p>
           <a
             className="App-link"
